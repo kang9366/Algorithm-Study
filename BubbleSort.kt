225aeeea4dcebba454ab4dbbaedd58198aeaa19f
SeungGu Kang <alex9366@naver.com>
2022-07-11 23:03:17 +0900

    Bubble Sort (Kotlin)

--- BubbleSort.kt
+++ BubbleSort.kt
@@ -1,7 +1,14 @@
 fun main(){
-    val list = listOf<Int>(7, 4, 5, 1, 3)
-    for(i in list){
-        for(i in list){
+    val list = mutableListOf<Int>(7, 4, 5, 1, 3)
+    var temp: Int
+    for(i in 0..list.size-2){
+        for(i in 0..list.size-2){
+            if(list[i] > list[i+1]){
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
+            }
         }
     }
+    println(list)
 }
